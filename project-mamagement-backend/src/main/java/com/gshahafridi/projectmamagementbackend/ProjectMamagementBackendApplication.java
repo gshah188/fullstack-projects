c5b45a903121e347eced0b99ef95af64b157f317
gshah188 <gshah188@yahoo.com>
2020-07-21 12:40:18 +0500

    first commit

---
+++ project-mamagement-backend/src/main/java/com/gshahafridi/projectmamagementbackend/ProjectMamagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gshahafridi.projectmamagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMamagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMamagementBackendApplication.class, args);
+    }
+
+}
---
+++ project-mamagement-backend/src/test/java/com/gshahafridi/projectmamagementbackend/ProjectMamagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gshahafridi.projectmamagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMamagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
